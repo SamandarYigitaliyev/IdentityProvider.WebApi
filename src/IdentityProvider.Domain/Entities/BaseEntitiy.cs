3e4103a52bf216e6c3d6067bd5cfe222d1312a6f
SamandarYigitaliyev <programmist4467@gmail.com>
2023-09-24 22:57:10 +0500

    Drop Backups

--- src/IdentityProvider.Domain/Entities/BaseEntitiy.cs
+++ src/IdentityProvider.Domain/Entities/BaseEntitiy.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +9,9 @@ using System.Threading.Tasks;
 namespace IdentityProvider.Domain.Entities
 {
     public class BaseEntitiy
-    {   
-        
+    {
+        [Key,Required]
+        [Column("id")]
         public long Id { get; set; }
     }
 }
--- src/IdentityProvider.Domain/Entities/Users/Users.cs
+++ src/IdentityProvider.Domain/Entities/Users/Users.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IdentityProvider.Domain.Entities.Users
 {
-    internal class Users
+    [Table("users")]
+    public class Users:Auditable
     {
+        [Required]
+        [Column("first_name")]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [Column("last_name")]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [Column("phone_number")]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        [Column("Salt")]
+        public string Salt { get; set; } = string.Empty;
+        [Required]
+        [Column("password_hash")]
+        public string PasswordHash { get; set; } = string.Empty;
+        [Column("provider")]
+        public string Provider { get; set; } = string.Empty;
+        [Column("provider_confirme")]
+        public string ProviderConfirme { get; set; } = string.Empty;
     }
 }
