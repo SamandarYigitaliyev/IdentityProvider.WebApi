f53938916c5f03194f3db369203695be18f65031
SamandarYigitaliyev <programmist4467@gmail.com>
2023-09-25 01:40:33 +0500

    DbContext created

---
+++ src/IdentityProvider.WebApi/Configurations/LayerConfigurations/DataAccessConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using IdentityProvider.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace IdentityProvider.WebApi.Configurations.LayerConfigurations
+{
+    public static class DataAccessConfiguration
+    {
+        public static void ConfigureDataAccess(this WebApplicationBuilder builder)
+        {
+            string connection = builder.Configuration.GetConnectionString("DatabaseConnectionString")!;
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(connection);
+            });
+        }
+    }
+}
