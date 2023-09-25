0bfe5f5f81a22fd1554d25976a8801299988d1f2
SamandarYigitaliyev <programmist4467@gmail.com>
2023-09-25 10:24:32 +0500

    Migration Created

---
+++ src/IdentityProvider.DataAccess/Migrations/20230925052045_InitialCreate.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityProvider.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    phone_number = table.Column<string>(type: "text", nullable: false),
+                    Salt = table.Column<string>(type: "text", nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false),
+                    provider = table.Column<string>(type: "text", nullable: false),
+                    provider_confirme = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_access_token",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    is_revoked = table.Column<bool>(type: "boolean", nullable: false),
+                    access_token = table.Column<string>(type: "text", nullable: false),
+                    expired_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_access_token", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_access_token_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_login",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserID = table.Column<Guid>(type: "uuid", nullable: false),
+                    provider_name = table.Column<string>(type: "text", nullable: false),
+                    provider_key = table.Column<string>(type: "text", nullable: false),
+                    login_provider = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_login", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_login_users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_login_device",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ip = table.Column<string>(type: "text", nullable: false),
+                    device_name = table.Column<string>(type: "text", nullable: false),
+                    device_key_name = table.Column<string>(type: "text", nullable: false),
+                    device_version = table.Column<double>(type: "double precision", nullable: false),
+                    os = table.Column<string>(type: "text", nullable: false),
+                    browser = table.Column<string>(type: "text", nullable: false),
+                    browser_version = table.Column<string>(type: "text", nullable: false),
+                    os_version = table.Column<string>(type: "text", nullable: false),
+                    country = table.Column<string>(type: "text", nullable: false),
+                    region = table.Column<string>(type: "text", nullable: false),
+                    lattitude = table.Column<string>(type: "text", nullable: false),
+                    longitude = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_login_device", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_login_device_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_refresh_token",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    refresh_token = table.Column<string>(type: "text", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user_refresh_token", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_user_refresh_token_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_access_token_user_id",
+                table: "user_access_token",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_login_UserID",
+                table: "user_login",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_login_device_user_id",
+                table: "user_login_device",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_user_refresh_token_user_id",
+                table: "user_refresh_token",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_access_token");
+
+            migrationBuilder.DropTable(
+                name: "user_login");
+
+            migrationBuilder.DropTable(
+                name: "user_login_device");
+
+            migrationBuilder.DropTable(
+                name: "user_refresh_token");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
